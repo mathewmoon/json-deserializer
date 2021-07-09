8478513e78b8cf902530c0dbcdb1c8289815aeb2
Mathew Moon <mmoon@quinovas.com>
2021-07-09 08:51:23 -0500

    Make into a class

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='json-deserializer',
-    version='0.0.5',
+    version='0.0.6',
     description='Attempts to correctly deserialize objects that json decoder cannot.',
     author='Mathew Moon',
     author_email='me@mathewmoon.net',
